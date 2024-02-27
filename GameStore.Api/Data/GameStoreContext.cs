fa3c5606f033de2839ea7cad577a8c53c14c66cb
A-San96 <assanediatta96@gmail.com>
2024-02-27 14:42:40 +0000

    feat: add sqlite database and ef core

---
+++ GameStore.Api/Data/GameStoreContext.cs
@@ -0,0 +1,13 @@
+﻿using GameStore.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Api.Data;
+
+public class GameStoreContext : DbContext
+{
+    public GameStoreContext(DbContextOptions<GameStoreContext> options) : base(options)
+    { }
+
+    public DbSet<Game> Games => Set<Game>();
+    public DbSet<Genre> Genres => Set<Genre>();
+}
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,10 +1,15 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var ConnString = builder.Configuration.GetConnectionString("GameStore");
+builder.Services.AddSqlite<GameStoreContext>(ConnString);
 
 var app = builder.Build();
 
 app.MapGamesEndpoints();
 
+app.MigrateDb();
 
 app.Run();
