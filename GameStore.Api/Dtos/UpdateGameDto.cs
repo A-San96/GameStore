68319da9e0d7e6a7226ca3c5c59c938c4f362090
A-San96 <assanediatta96@gmail.com>
2024-03-03 13:17:12 +0000

    feat: add update with db context

--- GameStore.Api/Dtos/UpdateGameDto.cs
+++ GameStore.Api/Dtos/UpdateGameDto.cs
@@ -4,6 +4,6 @@ namespace GameStore.Api.Dtos;
 
 public record class UpdateGameDto(
     [Required][StringLength(50)] string Name,
-    [Required][StringLength(50)] string Genre,
+    [Required] int GenreId,
     [Range(1, 100)] decimal Price,
     DateOnly ReleaseDate);
--- GameStore.Api/Mapping/GameMapping.cs
+++ GameStore.Api/Mapping/GameMapping.cs
@@ -16,6 +16,18 @@ public static class GameMapping
         };
     }
 
+    public static Game ToEntity(this UpdateGameDto game, int id)
+    {
+        return new Game()
+        {
+            Id = id,
+            Name = game.Name,
+            GenreId = game.GenreId,
+            Price = game.Price,
+            ReleaseDate = game.ReleaseDate
+        };
+    }
+
     public static GameSummaryDto ToGameSummaryDto(this Game game)
     {
         GameSummaryDto gameSummaryDto = new(
